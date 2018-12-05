37e5a50709ee6618f8664cae0d728fdc7384ff55
Paul Walsh <twentytwentyk@live.com>
2018-12-05 13:12:02 -0500

    express routes reconfigured for postgres stickers api

---
+++ server/api/stickers.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router();
+
+const queries = require('../db/queries');
+
+router.get('/', (req, res) => {
+  queries.getAll().then(stickers => {
+    res.json(stickers);
+  });
+});
+
+module.exports = router;
